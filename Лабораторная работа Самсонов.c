39785860c371e2399f21af92d7c1fd64c510384b
dominatorr1337 <dominatorr1337@gmail.com>
2025-10-17 19:22:12 +0300

    Add files via upload

---
+++ Лабораторная работа Самсонов.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SEQUENCE_LENGTH 100
+
+void find_sequences(int target, int numbers[], int m, 
+                   int current_sequence[], int current_length,
+                   int last_num) {
+    
+    if (target == 0) {
+
+        printf("[");
+        for (int i = 0; i < current_length; i++) {
+            printf("%d", current_sequence[i]);
+            if (i < current_length - 1) {
+                printf(", ");
+            }
+        }
+        printf("]\n");
+        return;
+    }
+
+    if (target < 0) {
+        return;
+    }
+
+    for (int i = 0; i < m; i++) {
+        int num = numbers[i];
+
+        if (num != last_num) {
+
+            current_sequence[current_length] = num;
+
+            find_sequences(target - num, numbers, m, 
+                          current_sequence, current_length + 1,
+                          num);
+        }
+    }
+}
+
+int main(argv) {
+    int N, M;
+
+    printf("Введите число N: ");
+    scanf("%d", &N);
+    
+    printf("Введите количество элементов M: ");
+    scanf("%d", &M);
+
+    int* S = (int*)malloc(M * sizeof(int));
+    if (S == NULL) {
+        printf("Ошибка выделения памяти!\n");
+        return 1;
+    }
+    
+    printf("Введите элементы множества S:\n");
+    for (int i = 0; i < M; i++) {
+        printf("Элемент %d: ", i + 1);
+        scanf("%d", &S[i]);
+    }
+   
+    int* current_sequence = (int*)malloc(MAX_SEQUENCE_LENGTH * sizeof(int));
+    if (current_sequence == NULL) {
+        printf("Ошибка выделения памяти!\n");
+        free(S);
+        return 1;
+    }
+    
+    printf("\nВсе последовательности чисел из {");
+    for (int i = 0; i < M; i++) {
+        printf("%d", S[i]);
+        if (i < M - 1) printf(", ");
+    }
+    printf("}, сумма которых равна %d:\n", N);
+
+    find_sequences(N, S, M, current_sequence, 0, -1);
+
+    free(S);
+    free(current_sequence);
+    
+    return 0;
+}
