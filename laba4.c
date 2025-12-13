925871d85201c5d3087ac7f2f492412eb629538e
dominatorr1337 <dominatorr1337@gmail.com>
2025-12-13 05:04:22 +0300

    laba4.c

--- laba4.c
+++ laba4.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
-void bubble_sort(int arr[], int n) {
+void bubble_sort(int m[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+            if (m[j] > m[j+1]) {
+                int x = m[j];
+                m[j] = m[j+1];
+                m[j+1] = x;
             }
         }
     }
 }
 
-void insertion_sort(int arr[], int n) {
+void insertion_sort(int m[], int n) {
     for (int i = 1; i < n; i++) {
-        int key = arr[i];
+        int q = m[i];
         int j = i-1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j+1] = arr[j];
+        while (j >= 0 && m[j] > q) {
+            m[j+1] = m[j];
             j--;
         }
-        arr[j+1] = key;
+        m[j+1] = q;
     }
 }
 
-void merge(int arr[], int left, int mid, int right) {
+void merge(int m[], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
     int* L = (int*)malloc(n1 * sizeof(int));
@@ -35,83 +35,83 @@ void merge(int arr[], int left, int mid, int right) {
         printf("ошибка выделения памяти\n");
         return;
     }
-    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
-    for (int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
+    for (int i = 0; i < n1; i++) L[i] = m[left + i];
+    for (int i = 0; i < n2; i++) R[i] = m[mid + 1 + i];
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) arr[k++] = L[i++];
-        else arr[k++] = R[j++];
+        if (L[i] <= R[j]) m[k++] = L[i++];
+        else m[k++] = R[j++];
     }
-    while (i < n1) arr[k++] = L[i++];
-    while (j < n2) arr[k++] = R[j++];
+    while (i < n1) m[k++] = L[i++];
+    while (j < n2) m[k++] = R[j++];
     free(L);
     free(R);
 }
 
-void merge_sort(int arr[], int left, int right) {
+void merge_sort(int m[], int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
-        merge_sort(arr, left, mid);
-        merge_sort(arr, mid+1, right);
-        merge(arr, left, mid, right);
+        merge_sort(m, left, mid);
+        merge_sort(m, mid+1, right);
+        merge(m, left, mid, right);
     }
 }
 
-void generate_array(int arr[], int n) {
+void generate_array(int m[], int n) {
     for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 1000;
+        m[i] = rand() % 1000;
     }
 }
-void copy_array(int source[], int copied[], int n) {
+void copy_array(int ist[], int cop[], int n) {
     for (int i = 0; i < n; i++) {
-        copied[i] = source[i];
+        cop[i] = ist[i];
     }
 }
 
 int main() {
     srand(time(NULL));
-    int sizes[] = {10, 1000, 10000, 100000};
-    int num_sizes = 4;
+    int s[] = {10, 1000, 10000, 100000};
+    int ns = 4;
     printf("размер | пузырек | вставки | слияние\n");
-    for (int i = 0; i < num_sizes; i++) {
-        int n = sizes[i];
-        int* original = malloc(n * sizeof(int));
-        generate_array(original, n);
+    for (int i = 0; i < ns; i++) {
+        int n = s[i];
+        int* u = malloc(n * sizeof(int));
+        generate_array(u, n);
         printf("%6d | ", n);
         if (n <= 10000) {
-            int* arr = malloc(n * sizeof(int));
-            copy_array(original, arr, n);
+            int* m = malloc(n * sizeof(int));
+            copy_array(u, m, n);
             clock_t start = clock();
-            bubble_sort(arr, n);
+            bubble_sort(m, n);
             clock_t end = clock();
-            double time = (double)(end - start) / CLOCKS_PER_SEC;
+            double seconds = (double)(end - start) / CLOCKS_PER_SEC;
             printf("%7.3f | ", time);
-            free(arr);
+            free(m);
         } else {
             printf("  долго | ");
         }
         if (n <= 10000) {
             int* arr = malloc(n * sizeof(int));
-            copy_array(original, arr, n);
+            copy_array(u, m, n);
             clock_t start = clock();
-            insertion_sort(arr, n);
+            insertion_sort(m, n);
             clock_t end = clock();
-            double time = (double)(end - start) / CLOCKS_PER_SEC;
+            double seconds = (double)(end - start) / CLOCKS_PER_SEC;
             printf("%7.3f | ", time);
-            free(arr);
+            free(m);
         } else {
             printf("  долго | ");
         }
-        int* arr = malloc(n * sizeof(int));
-        copy_array(original, arr, n);
+        int* m = malloc(n * sizeof(int));
+        copy_array(u, m, n);
         clock_t start = clock();
-        merge_sort(arr, 0, n-1);
+        merge_sort(m, 0, n-1);
         clock_t end = clock();
         double time = (double)(end - start) / CLOCKS_PER_SEC;
         printf("%7.3f\n", time);
         
-        free(arr);
-        free(original);
+        free(m);
+        free(u);
     }
     return 0;
 }
